895ca1f25eb058c1f54aec03a23668e7c117663c
shon97 <wjdgns200297@gmail.com>
2022-03-31 20:42:15 +0900

    ADD: 카테고리 API

---
+++ models/CategoryDao.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const getCategory = async () => {
+  return await prisma.$queryRaw`
+      select 
+	      tc.id,
+	      tc.name as name,
+        lc.id as lessonId,
+        lc.name as lessonName,
+        lc.image as lessonImage
+      from 
+	      thema_categories tc
+      join 
+	      lesson_categories lc
+      on
+	      tc.id = lc.thema_category_id;
+      `;
+};
+
+module.exports = { getCategory };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// const userRoute = require('./userRoute')
+const CategoryRoute = require("./CategoryRoute");
 
 router.get("/", (req, res) =>
   res.status(200).json({ message: "Hello! You are connected." })
 );
 
-// router.use('/user', userRoute)
+router.use("/category", CategoryRoute);
 
 module.exports = router;
