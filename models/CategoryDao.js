bd0fc59fb6954b00f1b59a5aa7996d35bf9353ea
shon97 <wjdgns200297@gmail.com>
2022-04-04 01:49:08 +0900

    FIX: 카테고리 리스트트 API

--- models/CategoryDao.js
+++ models/CategoryDao.js
@@ -3,22 +3,19 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getCategory = async () => {
-  return await prisma.$queryRaw`
-    select 
-      tc.id as id,
-      tc.name as name,
-      json_arrayagg(lc.id) as lessonId,
-      json_arrayagg(lc.name) as lessonName,
-      json_arrayagg(lc.image) as lessonImage
-    from 
-      thema_categories tc
-    join 
-      lesson_categories lc
-    on
-      tc.id = lc.thema_category_id
-    group by
-      tc.id
-  `;
+  return await prisma.themaCategories.findMany({
+    select: {
+      id: true,
+      name: true,
+      LessonCategories: {
+        select: {
+          id: true,
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
 };
 
 module.exports = { getCategory };
