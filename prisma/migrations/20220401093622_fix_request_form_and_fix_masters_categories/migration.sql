04442e76ea9aba04d7321bece2bd4dd155fe061a
shon97 <wjdgns200297@gmail.com>
2022-04-05 13:40:47 +0900

    DEL: 마이그레이션 기록 삭제 후 재 init

--- prisma/migrations/20220401093622_fix_request_form_and_fix_masters_categories/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `masters_categories` ADD COLUMN `is_main` BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `request_form` ADD COLUMN `ended_at` DATETIME(3) NULL;
