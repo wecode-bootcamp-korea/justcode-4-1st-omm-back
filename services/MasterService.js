96df1853e09a6c964c60239a173ad494713a9027
shon97 <wjdgns200297@gmail.com>
2022-04-07 14:59:04 +0900

    ADD: 고수 프로필 API

--- services/MasterService.js
+++ services/MasterService.js
@@ -18,19 +18,19 @@ const signUp = async (
   try {
     // 사용자 입력값 검증
     if (name.length < 2) {
-      throw await errorGenerator({ statusCode:400, message: "WRONG_NAME"})
+      throw await errorGenerator({ statusCode: 400, message: "WRONG_NAME" });
     }
 
     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!email.match(mailFormat)) {
-      throw await errorGenerator({ statusCode:400, message: "WRONG_EMAIL" })
+      throw await errorGenerator({ statusCode: 400, message: "WRONG_EMAIL" });
     }
 
     const pwFormat = /^[A-Za-z0-9+]{8,50}$/;
     if (!password.match(pwFormat)) {
       throw await errorGenerator({
         statusCode: 400,
-        message: "WRONG_PASSWORD"
+        message: "WRONG_PASSWORD",
       });
     }
 
@@ -38,66 +38,92 @@ const signUp = async (
     if (!phoneNumber.match(phoneFormat)) {
       throw await errorGenerator({
         statusCode: 400,
-        message: "WRONG_PHONE_NUMBER"
+        message: "WRONG_PHONE_NUMBER",
       });
     }
 
     if (typeof token === "undefined") {
       // 이메일과 번호로 가입된 사용자인지 확인
-      const userInfo = await UserDao.findUserInfo(email, phoneNumber)
+      const userInfo = await UserDao.findUserInfo(email, phoneNumber);
 
       // 로그인을 하지 않았지만, 일반 회원이나 고수로 이미 가입한 사용자인지 확인
       if (userInfo.length !== 0) {
-        const isExistingMaster = await MasterDao.findMasterInfo(userInfo[0].id)
+        const isExistingMaster = await MasterDao.findMasterInfo(userInfo[0].id);
         if (isExistingMaster.length !== 0) {
-            throw await errorGenerator({ statusCode:400, message: "EXISTING_MASTER" })
+          throw await errorGenerator({
+            statusCode: 400,
+            message: "EXISTING_MASTER",
+          });
         } else {
-            throw await errorGenerator({ statusCode:400, message: "EXISTING_USER!_PLEASE_LOGIN" })
+          throw await errorGenerator({
+            statusCode: 400,
+            message: "EXISTING_USER!_PLEASE_LOGIN",
+          });
         }
       }
 
       // 일반 회원으로 가입한 적이 없고 고수로 가입 신청한 경우, user 테이블에 추가
-      const hashedPW = bc.hashSync(password,bc.genSaltSync())
+      const hashedPW = bc.hashSync(password, bc.genSaltSync());
 
-      await UserDao.createUserDirectMaster(name, email, hashedPW, phoneNumber)
-
+      await UserDao.createUserDirectMaster(name, email, hashedPW, phoneNumber);
     } else {
-
       // 로그인한 토큰을 가지고 유저가 맞는지 확인
       try {
-        const isValidUser = jwt.verify(token, process.env.SECRET_KEY)
-        await UserDao.insertPhoneNum(isValidUser.id, phoneNumber)
+        const isValidUser = jwt.verify(token, process.env.SECRET_KEY);
+        await UserDao.insertPhoneNum(isValidUser.id, phoneNumber);
       } catch (error) {
-        throw await errorGenerator({ statusCode:400, message: "INVALID_USER" })
+        throw await errorGenerator({
+          statusCode: 400,
+          message: "INVALID_USER",
+        });
       }
     }
 
     // user ID 가져오기
-    const userInfo = await UserDao.getUserByEmail(email)
-    const userID = userInfo[0].id
-    
+    const userInfo = await UserDao.getUserByEmail(email);
+    const userID = userInfo[0].id;
+
     // master 테이블에 추가
     const master = await MasterDao.createMaster(userID, name);
 
     await MasterDao.insertMasterCat(master.id, lessonCatID);
 
-    const masterAddress = await MasterDao.findMasterAddress(address, detailAddress);
+    const masterAddress = await MasterDao.findMasterAddress(
+      address,
+      detailAddress
+    );
 
-    await MasterDao.insertMasterAddress(master.id, masterAddress.addressID[0].id, masterAddress.detailAddressID[0].id);
+    await MasterDao.insertMasterAddress(
+      master.id,
+      masterAddress.addressID[0].id,
+      masterAddress.detailAddressID[0].id
+    );
 
-    return master
+    return master;
   } catch (error) {
-    throw await error
+    throw await error;
   }
 };
 
 const sendMasterReview = async (masterID) => {
   try {
     const masterReivew = MasterDao.findMasterReview(id);
-    return masterReivew        
+    return masterReivew;
   } catch (error) {
-    throw await error
+    throw await error;
   }
-}
+};
 
-module.exports = { signUp, sendMasterReview }
+const getMasterProfile = async (masterId) => {
+  return await MasterDao.getMasterProfile(masterId);
+};
+
+const setMasterProfile = async (params) => {
+  return await MasterDao.setMasterProfile(params);
+};
+module.exports = {
+  signUp,
+  sendMasterReview,
+  getMasterProfile,
+  setMasterProfile,
+};
