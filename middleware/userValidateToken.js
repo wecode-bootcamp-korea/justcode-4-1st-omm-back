409748979205f5efc4bd5596afbe514d98d3548d
shw779 <shw779@naver.com>
2022-04-07 19:19:35 +0900

    [FIX]validate 체크 마스터 유저로 분리

---
+++ middleware/userValidateToken.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const errorGenerator = require("../utils/errorGenerator");
+const MasterService = require("../services/MasterService");
+const UserService = require("../services/UserService");
+const { SECRET_KEY } = process.env;
+
+const validateToken = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    console.log("hey", token);
+    if (!token || token === 'null' || token === undefined) {
+      throw await errorGenerator({
+        statusCode: 400,
+        message: "TOKEN_UNDEFINED",
+      });
+    }
+    const { id } = jwt.verify(token, SECRET_KEY);
+
+    if (!id) {
+      throw await errorGenerator({
+        statusCode: 400,
+        message: "INCORRECT_TOKEN",
+      });
+    }
+
+    const findUser = await UserService.getUserByUserId(id);
+
+    if (!findUser) {
+      throw await errorGenerator({
+        statusCode: 404,
+        message: "USER_NOT_FOUND",
+      });
+    }
+    req.master = findUser;
+
+    next();
+  } catch (err) {
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+module.exports = validateToken;
--- routes/FormRoute.js
+++ routes/FormRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const validateToken = require("../middleware/userValidateToken");
 
 const FormController = require("../controllers/FormController");
 
 // GET
-router.get("/questions/:lessonId", FormController.getQuestions);
+router.get("/questions/:lessonId",validateToken, FormController.getQuestions);
 // POST
-router.post("/questions/complete", FormController.postQuestions);
+router.post("/questions/complete",validateToken, FormController.postQuestions);
 
 module.exports = router;
