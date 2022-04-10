94dc3f8abe5a5eb119411a2e3d11cbd1fc262e59
0teklee <leetekwoo@gmail.com>
2022-04-11 00:30:21 +0900

    [FIX] master-detail 추가 정보 전달, ReviewDao 수정

--- models/ReviewDao.js
+++ models/ReviewDao.js
@@ -2,7 +2,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-
 const sendPreview = async (masterId) => {
   try {
     return await prisma.$queryRaw`
@@ -16,15 +15,17 @@ const sendPreview = async (masterId) => {
   } catch (error) {
     throw await errorGenerator({ statusCode: 500, message: "SERVER_ERROR" });
   }
-}
+};
 
 const sendReviews = async (masterId) => {
   try {
     return await prisma.$queryRaw`
-        SELECT r.user_id AS userId, r.grade, r.comment
+        SELECT r.user_id AS userId, r.grade, r.comment, r.created_at, u.name
         FROM reviews r
         JOIN masters m
-        ON r.master_id = m.id
+        JOIN users u
+        ON r.master_id = m.id 
+        AND u.id = r.user_id
         WHERE m.id = ${masterId};
         `;
   } catch (error) {
