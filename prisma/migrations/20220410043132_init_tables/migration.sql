acb0783dfcf92cc7c83f3b92e68d98fade7f6c9f
yeonjoo_kim <jenny.likes.breeze@gmail.com>
2022-04-10 13:57:16 +0900

    [ADD & FIX] master_posts, review_images 테이블 추가 및 오류 수정

--- prisma/migrations/20220410043132_init_tables/migration.sql
+++ prisma/migrations/20220410043132_init_tables/migration.sql
@@ -4,17 +4,14 @@ CREATE TABLE `users` (
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `user_image` VARCHAR(191) NOT NULL,
-    `phone_number` VARCHAR(191) NOT NULL,
+    `user_image` VARCHAR(191) NULL,
+    `phone_number` VARCHAR(191) NULL,
     `is_deleted` BOOLEAN NOT NULL DEFAULT false,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `deleted_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-<<<<<<< HEAD:prisma/migrations/20220329103616_first_setting_tables/migration.sql
-=======
     UNIQUE INDEX `users_email_key`(`email`),
->>>>>>> dc48598db245093b6973cc118b54569174f10f12:prisma/migrations/20220407025751_init_table/migration.sql
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -22,20 +19,14 @@ CREATE TABLE `users` (
 CREATE TABLE `masters` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
-<<<<<<< HEAD:prisma/migrations/20220329103616_first_setting_tables/migration.sql
-    `intro` VARCHAR(191) NOT NULL,
-    `location` VARCHAR(191) NOT NULL,
-=======
     `address_id` INTEGER NOT NULL,
     `detail_address_id` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
-    `master_image` VARCHAR(191) NULL,
     `intro` VARCHAR(191) NULL,
     `start_time` VARCHAR(191) NULL,
     `end_time` VARCHAR(191) NULL,
     `work_experience` INTEGER NULL,
     `employee_number` INTEGER NULL,
->>>>>>> dc48598db245093b6973cc118b54569174f10f12:prisma/migrations/20220407025751_init_table/migration.sql
     `is_deleted` BOOLEAN NOT NULL DEFAULT false,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -47,7 +38,7 @@ CREATE TABLE `masters` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `theme_categories` (
+CREATE TABLE `thema_categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -59,12 +50,13 @@ CREATE TABLE `theme_categories` (
 -- CreateTable
 CREATE TABLE `lesson_categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `theme_category_id` INTEGER NOT NULL,
+    `thema_category_id` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    INDEX `theme_category_id`(`theme_category_id`),
+    INDEX `thema_category_id`(`thema_category_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -100,6 +92,7 @@ CREATE TABLE `request_form` (
     `question_id` INTEGER NOT NULL,
     `choice_question_id` INTEGER NOT NULL,
     `lesson_category_id` INTEGER NOT NULL,
+    `ended_at` DATETIME(3) NULL,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
@@ -126,33 +119,44 @@ CREATE TABLE `reviews` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `masters_categories` (
+CREATE TABLE `review_images` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `request_form_id` INTEGER NOT NULL,
-    `master_id` INTEGER NOT NULL,
+    `review_id` INTEGER NOT NULL,
+    `review_image` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    INDEX `master_id`(`master_id`),
-    INDEX `request_form_id`(`request_form_id`),
+    INDEX `review_id`(`review_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-<<<<<<< HEAD:prisma/migrations/20220329103616_first_setting_tables/migration.sql
-CREATE TABLE `masters_request_form` (
+CREATE TABLE `masters_categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `request_form_id` INTEGER NOT NULL,
+    `lesson_category_id` INTEGER NOT NULL,
     `master_id` INTEGER NOT NULL,
+    `is_main` BOOLEAN NOT NULL DEFAULT false,
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     INDEX `master_id`(`master_id`),
-    INDEX `request_form_id`(`request_form_id`),
+    INDEX `lesson_category_id`(`lesson_category_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-=======
+-- CreateTable
+CREATE TABLE `master_posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `master_id` INTEGER NOT NULL,
+    `master_image_address` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `master_id`(`master_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `address` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
@@ -180,12 +184,11 @@ ALTER TABLE `masters` ADD CONSTRAINT `masters_user_id_fkey` FOREIGN KEY (`user_i
 -- AddForeignKey
 ALTER TABLE `masters` ADD CONSTRAINT `masters_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
->>>>>>> dc48598db245093b6973cc118b54569174f10f12:prisma/migrations/20220407025751_init_table/migration.sql
 -- AddForeignKey
 ALTER TABLE `masters` ADD CONSTRAINT `masters_detail_address_id_fkey` FOREIGN KEY (`detail_address_id`) REFERENCES `detail_address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `lesson_categories` ADD CONSTRAINT `lesson_categories_theme_category_id_fkey` FOREIGN KEY (`theme_category_id`) REFERENCES `theme_categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `lesson_categories` ADD CONSTRAINT `lesson_categories_thema_category_id_fkey` FOREIGN KEY (`thema_category_id`) REFERENCES `thema_categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `questions` ADD CONSTRAINT `questions_lesson_category_id_fkey` FOREIGN KEY (`lesson_category_id`) REFERENCES `lesson_categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -212,17 +215,16 @@ ALTER TABLE `reviews` ADD CONSTRAINT `reviews_user_id_fkey` FOREIGN KEY (`user_i
 ALTER TABLE `reviews` ADD CONSTRAINT `reviews_master_id_fkey` FOREIGN KEY (`master_id`) REFERENCES `masters`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `review_images` ADD CONSTRAINT `review_images_review_id_fkey` FOREIGN KEY (`review_id`) REFERENCES `reviews`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `masters_categories` ADD CONSTRAINT `masters_categories_master_id_fkey` FOREIGN KEY (`master_id`) REFERENCES `masters`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `masters_categories` ADD CONSTRAINT `masters_categories_request_form_id_fkey` FOREIGN KEY (`request_form_id`) REFERENCES `request_form`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `masters_categories` ADD CONSTRAINT `masters_categories_lesson_category_id_fkey` FOREIGN KEY (`lesson_category_id`) REFERENCES `lesson_categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-<<<<<<< HEAD:prisma/migrations/20220329103616_first_setting_tables/migration.sql
-ALTER TABLE `masters_request_form` ADD CONSTRAINT `masters_request_form_master_id_fkey` FOREIGN KEY (`master_id`) REFERENCES `masters`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `master_posts` ADD CONSTRAINT `master_posts_master_id_fkey` FOREIGN KEY (`master_id`) REFERENCES `masters`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `masters_request_form` ADD CONSTRAINT `masters_request_form_request_form_id_fkey` FOREIGN KEY (`request_form_id`) REFERENCES `request_form`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-=======
 ALTER TABLE `detail_address` ADD CONSTRAINT `detail_address_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `address`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
->>>>>>> dc48598db245093b6973cc118b54569174f10f12:prisma/migrations/20220407025751_init_table/migration.sql
